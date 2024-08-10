fda9e50ef6008b44b651d39c284308b95ce5ead5
Smartlizardpy <ozan.kaygusuz11@gmail.com>
2024-08-10 16:00:27 +0300

    modified:   index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = 8080;
+const port = 3000;
 
 // Middleware to serve static files
 app.use(express.static(__dirname));
